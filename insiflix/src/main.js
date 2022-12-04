949591b6d6b3f2b15e9ded0d054b93b3b300b57b
65Juko <thejulianjuko@gmail.com>
2022-12-05 00:32:08 +0100

    added socket io

--- insiflix/src/main.js
+++ insiflix/src/main.js
@@ -1,6 +1,12 @@
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from "vue";
+import App from "./App.vue";
+import socketio from "socket.io-client";
+import VueSocketIO from "vue-socket.io";
+
+export const SocketInstance = socketio("http://localhost:5000");
+
+Vue.use(VueSocketIO, SocketInstance);
 
 new Vue({
-  render: (h) => h(App),
-}).$mount('#app');
+	render: h => h(App),
+}).$mount("#app");
