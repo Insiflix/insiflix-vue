404536f1cf105c968801cee2da04911f27c38eb9
fabianmerkl <108922830+fabianmerkl@users.noreply.github.com>
2023-01-05 03:57:54 +0100

    faaaaaaarbe

--- insiflix/src/components/CustomSelect.vue
+++ insiflix/src/components/CustomSelect.vue
@@ -1,71 +1,78 @@
 <template>
-    <div>
-        <div class="select-menu">
-            <div class="select-btn">
-                <div class="icon-flex">
-                    <i id="renderedIcon" class="bi bi-youtube" style="color: rgb(245, 0, 0);"></i>
-                    <span class="sBtn-text">Youtube</span>
-                </div>
-                    <i id="chevron" class="bi bi-chevron-down" style="font-size: 1.25rem;"></i>
-            </div>
-
-            <ul class="options">
-                <li class="option">
-                    <i class="bi bi-youtube" style="color: rgb(245, 0, 0);"></i>
-                    <span class="option-text">Youtube</span>
-                </li>
-                <li class="option">
-                    <i class="bi bi-file-earmark-arrow-up" style="color: #0E76A8;"></i>
-                    <span class="option-text">File</span>
-                </li>
-            </ul>
+  <div>
+    <div class="select-menu">
+      <div class="select-btn">
+        <div class="icon-flex">
+          <i
+            id="renderedIcon"
+            class="bi bi-youtube"
+            style="color: rgb(245, 0, 0)"
+          ></i>
+          <span class="sBtn-text">Youtube</span>
         </div>
+        <i
+          id="chevron"
+          class="bi bi-chevron-down"
+          style="font-size: 1.25rem"
+        ></i>
+      </div>
+
+      <ul class="options">
+        <li class="option">
+          <i class="bi bi-youtube" style="color: rgb(245, 0, 0)"></i>
+          <span class="option-text">Youtube</span>
+        </li>
+        <li class="option">
+          <i class="bi bi-file-earmark-arrow-up" style="color: #0e76a8"></i>
+          <span class="option-text">File</span>
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            optionMenu: "",
-            selectBtn: "",
-            options: "",
-            sBtn_text: "",
-            renderedIcon: "",
+  data() {
+    return {
+      optionMenu: "",
+      selectBtn: "",
+      options: "",
+      sBtn_text: "",
+      renderedIcon: "",
+    };
+  },
+  mounted() {
+    this.optionMenu = document.querySelector(".select-menu");
+    this.selectBtn = this.optionMenu.querySelector(".select-btn");
+    this.options = this.optionMenu.querySelectorAll(".option");
+    this.sBtn_text = this.optionMenu.querySelector(".sBtn-text");
+    this.renderedIcon = document.querySelector("#renderedIcon");
+
+    this.selectBtn.addEventListener("click", () =>
+      this.optionMenu.classList.toggle("active")
+    );
+
+    this.options.forEach((option) => {
+      option.addEventListener("click", () => {
+        let selectedOption = option.querySelector(".option-text").innerText;
+        if (selectedOption == "Youtube") {
+          this.renderedIcon.classList.remove("bi-file-earmark-arrow-up");
+          this.renderedIcon.classList.add("bi-youtube");
+          this.renderedIcon.style.color = "rgb(245, 0, 0)";
+        } else {
+          this.renderedIcon.classList.add("bi-file-earmark-arrow-up");
+          this.renderedIcon.classList.remove("bi-youtube");
+          this.renderedIcon.style.color = "rgb(14, 118, 168)";
         }
-    },
-    mounted() {
-        this.optionMenu = document.querySelector(".select-menu");
-        this.selectBtn = this.optionMenu.querySelector(".select-btn");
-        this.options = this.optionMenu.querySelectorAll(".option");
-        this.sBtn_text = this.optionMenu.querySelector(".sBtn-text");
-        this.renderedIcon = document.querySelector("#renderedIcon");
+        this.sBtn_text.innerText = selectedOption;
+        this.$store.state.activeUploadComponent = selectedOption;
 
-        this.selectBtn.addEventListener("click", () =>
-        this.optionMenu.classList.toggle("active")
-        );
-
-        this.options.forEach((option) => {
-        option.addEventListener("click", () => {
-            let selectedOption = option.querySelector(".option-text").innerText;
-            if(selectedOption == "Youtube") {
-                this.renderedIcon.classList.remove("bi-file-earmark-arrow-up");
-                this.renderedIcon.classList.add("bi-youtube");
-                this.renderedIcon.style.color="rgb(245, 0, 0)";
-            }
-            else {
-                this.renderedIcon.classList.add("bi-file-earmark-arrow-up");
-                this.renderedIcon.classList.remove("bi-youtube");
-                this.renderedIcon.style.color="rgb(14, 118, 168)";
-            }
-            this.sBtn_text.innerText = selectedOption;
-            this.$store.state.activeUploadComponent = selectedOption
-
-            this.optionMenu.classList.remove("active");
-        });
-});
-    }
-}
+        this.optionMenu.classList.remove("active");
+      });
+    });
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -75,102 +82,102 @@ export default {
   box-sizing: border-box;
 }
 .sBtn-text {
-    color: white;
+  color: white;
 }
 .icon-flex {
-    display: flex;
-    align-items: center;
-    gap: 0.5rem;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
 }
 
 $width: 330px;
 
 .select-menu {
-    padding: 2rem;
-    padding-right:2rem;
-	max-width: $width;
-	.select-btn {
-        min-width: 10rem;
-		display: flex;
-		height: 55px;
-		background: rgb(51, 51, 51);
-		padding: 20px;
-		font-size: 18px;
-		font-weight: 400;
-		border-radius: 8px;
-		align-items: center;
-		cursor: pointer;
-		justify-content: space-between;
-		box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-        gap: .4rem;
-	}
-	.options {
-		position: absolute;
-		width: 164px;
-        overflow-y: auto;
-        max-height: 295px;
-		padding: 10px;
-		margin-top: 10px;
-		border-radius: 8px;
-		background: rgb(51, 51, 51);;
-		box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-		animation-name: fadeInDown;
-		-webkit-animation-name: fadeInDown;
-		animation-duration: 0.35s;
-		animation-fill-mode: both;
-		-webkit-animation-duration: 0.35s;
-		-webkit-animation-fill-mode: both;
-		.option {
-			display: flex;
-			height: 55px;
-			cursor: pointer;
-			padding: 0 16px;
-			border-radius: 8px;
-			align-items: center;
-			background: rgb(51, 51, 51);;
-			&:hover {
-				background: rgb(75, 75, 75);;
-			}
-			i {
-				font-size: 25px;
-				margin-right: 12px;
-			}
-			.option-text {
-				font-size: 18px;
-				color: white;
-			}
-		}
-	}
+  padding: 2rem;
+  padding-right: 2rem;
+  max-width: $width;
+  .select-btn {
+    min-width: 10rem;
+    display: flex;
+    height: 55px;
+    background: rgb(51, 51, 51);
+    padding: 20px;
+    font-size: 18px;
+    font-weight: 400;
+    border-radius: 8px;
+    align-items: center;
+    cursor: pointer;
+    justify-content: space-between;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+    gap: 0.4rem;
+  }
+  .options {
+    position: absolute;
+    width: 164px;
+    overflow-y: auto;
+    max-height: 295px;
+    padding: 10px;
+    margin-top: 10px;
+    border-radius: 8px;
+    background: rgb(20, 20, 20);
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    animation-name: fadeInDown;
+    -webkit-animation-name: fadeInDown;
+    animation-duration: 0.35s;
+    animation-fill-mode: both;
+    -webkit-animation-duration: 0.35s;
+    -webkit-animation-fill-mode: both;
+    .option {
+      display: flex;
+      height: 55px;
+      cursor: pointer;
+      padding: 0 16px;
+      border-radius: 8px;
+      align-items: center;
+      background: rgb(20, 20, 20);
+      &:hover {
+        background: rgb(75, 75, 75);
+      }
+      i {
+        font-size: 25px;
+        margin-right: 12px;
+      }
+      .option-text {
+        font-size: 18px;
+        color: white;
+      }
+    }
+  }
 }
 #chevron {
-    color: white;
+  color: white;
 }
 .select-btn {
-    #renderedIcon {
-        font-size: 25px;
-    }
-	i:not(#renderedIcon) {
-		font-size: 25px;
-		transition: 0.3s;
-	}
+  #renderedIcon {
+    font-size: 25px;
+  }
+  i:not(#renderedIcon) {
+    font-size: 25px;
+    transition: 0.3s;
+  }
 }
 .select-menu.active {
-	.select-btn {
-		#chevron {
-			transform: rotate(-180deg);
-		}
-	}
-	.options {
-		display: block;
-		opacity: 0;
-		z-index: 100;
-		animation-name: fadeInUp;
-		-webkit-animation-name: fadeInUp;
-		animation-duration: 0.4s;
-		animation-fill-mode: both;
-		-webkit-animation-duration: 0.4s;
-		-webkit-animation-fill-mode: both;
-	}
+  .select-btn {
+    #chevron {
+      transform: rotate(-180deg);
+    }
+  }
+  .options {
+    display: block;
+    opacity: 0;
+    z-index: 100;
+    animation-name: fadeInUp;
+    -webkit-animation-name: fadeInUp;
+    animation-duration: 0.4s;
+    animation-fill-mode: both;
+    -webkit-animation-duration: 0.4s;
+    -webkit-animation-fill-mode: both;
+  }
 }
 
 @keyframes fadeInUp {
